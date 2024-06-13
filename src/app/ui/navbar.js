7c1a735702cd8107cfa7073bdef630df91d2610f
Alex Guerra <alexander.a.guerra@gmail.com>
2024-06-13 17:23:40 -0500

    chore: Remove SpeedInsights component from layout.js and add Navbar component to page.js

---
+++ src/app/ui/navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Link from 'next/link'
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-800 text-white py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <h1 className="text-2xl font-bold">Guerra Codes</h1>
+                <ul className="flex">
+                    <li className="ml-4">
+                        <Link href="/resume" className="p-2">
+                            Resume
+                        </Link>
+                    </li>
+                    <li className="ml-4">
+                        <a href="/projects" className="p-2">
+                            Projects
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
